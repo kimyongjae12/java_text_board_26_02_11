ea56573dea0aca274d077cf61ca64d7d255d03f6
kimyongjae12 <vvv2548@gmail.com>
2026-02-11 18:06:21 +0900

    요구사항1 완료

--- src/main/java/com/kyj/Main.java
+++ src/main/java/com/kyj/Main.java
@@ -1,9 +1,25 @@
 package com.kyj;
 
 
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.printf("Hello and welcome!");
+    Scanner sc = new Scanner(System.in);
+    System.out.println("== 자바 게시판 시작==");
 
+    while(true){
+      System.out.print("명령) ");
+      String cmd = sc.nextLine();
+
+      if(cmd.equals("exit")) {
+        System.out.println("프로그램을 종료합니다.");
+        break;
+      }
+      System.out.println("입력받은 명령어 : " + cmd);
+    }
+    System.out.println("== 자바 게시판 종료");
+
+    sc.close();
   }
 }
