18f1bcf0788449a19a26ad9c282b7038a68f6dda
kimyongjae12 <vvv2548@gmail.com>
2026-02-12 17:54:29 +0900

    step2: /usr/article/detail/1 에서 선택한 번호 가져오기

--- src/main/java/com/kyj/Main.java
+++ src/main/java/com/kyj/Main.java
@@ -4,6 +4,7 @@ package com.kyj;
 import com.kyj.domain.article.Article;
 
 import javax.naming.PartialResultException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -40,7 +41,12 @@ public class Main {
 
 
         System.out.printf("%d번 게시물이 등록 되었습니다.\n",id);
-      } else if(cmd.equals("/usr/article/detail")){
+      } else if(cmd.startsWith("/usr/article/detail")){
+        String[] urlBits = cmd.trim().split("/");
+        System.out.println(Arrays.toString(urlBits));
+
+        int id = Integer.parseInt(urlBits[4]);
+
         Article article = lastArticle;
 
         if(article == null){
@@ -48,7 +54,7 @@ public class Main {
           continue;
         }
 
-        System.out.printf("== %d번 게시물 상세보기 ==\n", article.id);
+        System.out.printf("== %d번 게시물 상세보기 ==\n", id);
         System.out.printf("번호 : %d\n", article.id);
         System.out.printf("제목 : %s\n", article.title);
         System.out.printf("내용 : %s\n", article.content);
