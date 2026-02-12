9ab1709217b288d61bbc7f401439258fbe428c32
kimyongjae12 <vvv2548@gmail.com>
2026-02-12 16:38:46 +0900

    요구사항 2번 완료

--- src/main/java/com/kyj/Main.java
+++ src/main/java/com/kyj/Main.java
@@ -11,11 +11,26 @@ public class Main {
     while(true){
       System.out.print("명령) ");
       String cmd = sc.nextLine();
+      if(cmd.equals("/usr/article/write")) {
+        System.out.println("== 게시물 작성==");
+        System.out.print("제목 :");
+        String title = sc.nextLine();
 
-      if(cmd.equals("exit")) {
+        System.out.print("내용 :");
+        String content = sc.nextLine();
+
+        int id = 1;
+
+        System.out.printf("%d번 게시물이 등록 되었습니다.\n",id);
+      }
+      else if(cmd.equals("exit")) {
         System.out.println("프로그램을 종료합니다.");
         break;
       }
+      else {
+        System.out.println("명령어 확인 후 다시 입력해주세요.");
+      }
+
       System.out.println("입력받은 명령어 : " + cmd);
     }
     System.out.println("== 자바 게시판 종료");
