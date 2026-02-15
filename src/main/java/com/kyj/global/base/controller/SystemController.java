71595046b593cb6ecc18950456bf91e662138d33
kimyongjae12 <vvv2548@gmail.com>
2026-02-15 15:09:08 +0900

    명령어에 대한 유효성 검사 처리

--- src/main/java/com/kyj/global/base/controller/SystemController.java
+++ src/main/java/com/kyj/global/base/controller/SystemController.java
@@ -22,7 +22,12 @@ public class SystemController {
 
     while (true) {
       System.out.print("명령) ");
-      String cmd = sc.nextLine();
+      String cmd = sc.nextLine().trim();
+
+      if(cmd.trim().isEmpty()){
+        System.out.println("명령어를 입력해주세요.");
+        continue;
+      }
 
       if(cmd.equals("exit")){
         System.out.println("프로그램을 종료합니다.");
@@ -31,7 +36,10 @@ public class SystemController {
       }
       String[] urlBits = cmd.trim().split("/");
 
-
+      if(urlBits.length<4){
+        System.out.println("올바른 명령어 형식이 아닙니다. (예: /usr/article/list");
+        continue;
+      }
       String urlPathUserType = urlBits[1];
       String urlPathUserResource = urlBits[2];
       String urlPathUserAction = urlBits[3];
@@ -39,6 +47,11 @@ public class SystemController {
 
       if (urlBits.length > 4) {
         urlPathVariable = urlBits[4];
+      }
+
+      if(!urlPathUserType.startsWith("user")){
+        System.out.println("명령어를 잘못입력하셨습니다.");
+
       }
 
       switch (urlPathUserType) {
@@ -54,11 +67,6 @@ public class SystemController {
             }
           }
         }
-        case "exit" -> {
-
-        }
-        default -> System.out.println("명령어 확인 후 다시 입력해주세요.");
-
       }
 
     }
