37f19dc26dd475e12205fb5f8fd797702d171456
kimyongjae12 <vvv2548@gmail.com>
2026-02-19 18:17:47 +0900

    Article 클래스에 lombok 적용

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,9 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    // 롬복 의존성 적용
+    implementation("org.projectlombok:lombok:1.18.42")
 }
 
 tasks.test {
--- src/main/java/com/kyj/domain/article/dto/Article.java
+++ src/main/java/com/kyj/domain/article/dto/Article.java
@@ -1,48 +1,24 @@
 package com.kyj.domain.article.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor // public Article(int id, String title, String content))
+@NoArgsConstructor  // public Article() {}
+@ToString
 public class Article {
   private static int lastId;
   private int id;
   private String title;
   private String content;
 
-  public Article(int id, String title, String content){
-    this.id = id;
-    this.title = title;
-    this.content = content;
+  static {
+    lastId = 0;
   }
 
   public Article(String title, String content) {
     this(++lastId, title, content);
 
-  }
-
-  public int getId() {
-    return id;
-  }
-
-  public void setId(int id) {
-    this.id = id;
-  }
-
-  public String getTitle() {
-    return title;
-  }
-
-  public void setTitle(String title) {
-    this.title = title;
-  }
-
-  public String getContent() {
-    return content;
-  }
-
-  public void setContent(String content) {
-    this.content = content;
-  }
-
-  @Override
-  public String toString() {
-    return "{id: %d, title: '%s', content: '%s'}".formatted(id, title, content);
   }
 }
