b835e4e12cd599e941d8fa01e02afebf4ba6a8be
kimyongjae12 <vvv2548@gmail.com>
2026-02-26 16:59:40 +0900

    url 쿼리스트링 분석을 Util 클래스를 통해 처리

--- src/test/java/QueryStringParsingTest.java
+++ src/test/java/QueryStringParsingTest.java
@@ -5,33 +5,38 @@ import java.util.Map;
 public class QueryStringParsingTest {
   public static void main(String[] args) {
     String url = "/usr/article/list?page=1&code=title&q=제목1 입니까? 여기는.. &cale=dasd";
-
-    Map<String, String> params = new HashMap<>();
-    String[] urlBits = url.split("\\?",2);
-    String queryString = urlBits[1];
-    //System.out.println(Arrays.toString(urlBits));
-//    System.out.println(queryString);
-
-    String[] queryBits = queryString.split("&");
-//    System.out.println(Arrays.toString(queryBits));
-
-    for(String queryBit : queryBits){
-      String[] queryBitBits = queryBit.split("=",2);
-//      System.out.println(Arrays.toString(queryBitBits));
-
-//      System.out.println(queryBitBits[0]);
-//      System.out.println(queryBitBits[1]);
-      String paramName = queryBitBits[0];
-      String paramValue = queryBitBits[1];
-
-      params.put(paramName, paramValue);
-    }
-
-    // System.out.println(params);
-    System.out.println("== 원하는 쿼리 데이터 가져오기 ==");
-    System.out.printf("page : %d\n", Integer.parseInt(params.get("page")));
-    System.out.printf("code : %s\n", params.get("code"));
-    System.out.printf("q : %s\n", params.get("q"));
+    String urlPath = Ut.getPathFromUrl(url);
+    Map<String, String> params = Ut.getParamFromUrl(url);
 
   }
-}
+  class Ut {
+
+    public static Map<String, String> getParamFromUrl(String url) {
+
+      Map<String, String> params = new HashMap<>();
+      String[] urlBits = url.split("\\?",2);
+
+      if(urlBits.length==1) return params;
+      
+      String queryString = urlBits[1];
+
+      String[] queryBits = queryString.split("&");
+
+      for(String queryBit : queryBits){
+        String[] queryBitBits = queryBit.split("=",2);
+
+        String paramName = queryBitBits[0];
+        String paramValue = queryBitBits[1];
+
+        params.put(paramName, paramValue);
+      }
+
+      return params;
+    }
+
+    public static String getPathFromUrl(String url) {
+      String[] urlBits = url.split("\\?",2);
+      return urlBits[0];
+    }
+  }
+    }
